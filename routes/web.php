e8aeca688f258bbccce47c9b2e63850d9344ad68
nuserV145 <noemdb@gmail.com>
2025-02-05 08:11:41 -0400

    wip 2025-02-05_08:11:41

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dashboard');
-})->name('dashboard');
+})->name('welcome');
 
 // Route::get('/chat', function () {
 //     return view('chat');
